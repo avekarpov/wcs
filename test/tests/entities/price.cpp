1c59e8a9569f259f6af0caf01601fe3f05f3a3ce
Artem Karpov <artem.ka1001pm@gmail.com>
2023-04-17 07:34:30 +0300

    Fix price test

--- test/tests/entities/price.cpp
+++ test/tests/entities/price.cpp
@@ -6,45 +6,45 @@ using namespace wcs;
 
 TEST_CASE("Price")
 {
-    CHECK(Price { 0.0 } == Price { 0.0 });
-    CHECK(Price { 0.0 } == Price { 0.0 + std::numeric_limits<double>::epsilon() });
-    CHECK(Price { 0.0 } == Price { 0.0 - std::numeric_limits<double>::epsilon() });
-    CHECK_FALSE(Price { 0.0 } == Price { 0.1 });
-    CHECK_FALSE(Price { 0.0 } == Price { 0.0 + 2 * std::numeric_limits<double>::epsilon() });
-    CHECK_FALSE(Price { 0.0 } == Price { 0.0 - 2 * std::numeric_limits<double>::epsilon() });
+    CHECK(Price { 1.0 } == Price { 1.0 });
+    CHECK(Price { 1.0 } == Price { 1.0 + std::numeric_limits<double>::epsilon() });
+    CHECK(Price { 1.0 } == Price { 1.0 - std::numeric_limits<double>::epsilon() });
+    CHECK_FALSE(Price { 1.0 } == Price { 0.1 });
+    CHECK_FALSE(Price { 1.0 } == Price { 1.0 + 2 * std::numeric_limits<double>::epsilon() });
+    CHECK_FALSE(Price { 1.0 } == Price { 1.0 - 2 * std::numeric_limits<double>::epsilon() });
     
-    CHECK(Price { 0.0 } != Price { 0.1 });
-    CHECK(Price { 0.0 } != Price { 0.0 + 2 * std::numeric_limits<double>::epsilon() });
-    CHECK(Price { 0.0 } != Price { 0.0 - 2 * std::numeric_limits<double>::epsilon() });
-    CHECK_FALSE(Price { 0.0 } != Price { 0.0 });
-    CHECK_FALSE(Price { 0.0 } != Price { 0.0 + std::numeric_limits<double>::epsilon() });
-    CHECK_FALSE(Price { 0.0 } != Price { 0.0 - std::numeric_limits<double>::epsilon() });
+    CHECK(Price { 1.0 } != Price { 0.1 });
+    CHECK(Price { 1.0 } != Price { 1.0 + 2 * std::numeric_limits<double>::epsilon() });
+    CHECK(Price { 1.0 } != Price { 1.0 - 2 * std::numeric_limits<double>::epsilon() });
+    CHECK_FALSE(Price { 1.0 } != Price { 1.0 });
+    CHECK_FALSE(Price { 1.0 } != Price { 1.0 + std::numeric_limits<double>::epsilon() });
+    CHECK_FALSE(Price { 1.0 } != Price { 1.0 - std::numeric_limits<double>::epsilon() });
     
-    CHECK(Price { 0.0 } < Price { 0.1 });
-    CHECK(Price { 0.0 } < Price { 0.0 + 2 * std::numeric_limits<double>::epsilon() });
-    CHECK_FALSE(Price { 0.0 } < Price { -0.1 });
-    CHECK_FALSE(Price { 0.0 } < Price { 0.0 + std::numeric_limits<double>::epsilon() });
-    CHECK_FALSE(Price { 0.0 } < Price { 0.0 - std::numeric_limits<double>::epsilon() });
-    CHECK_FALSE(Price { 0.0 } < Price { 0.0 - 2 * std::numeric_limits<double>::epsilon() });
+    CHECK(Price { 1.0 } < Price { 1.1 });
+    CHECK(Price { 1.0 } < Price { 1.0 + 2 * std::numeric_limits<double>::epsilon() });
+    CHECK_FALSE(Price { 1.0 } < Price { 0.1 });
+    CHECK_FALSE(Price { 1.0 } < Price { 1.0 + std::numeric_limits<double>::epsilon() });
+    CHECK_FALSE(Price { 1.0 } < Price { 1.0 - std::numeric_limits<double>::epsilon() });
+    CHECK_FALSE(Price { 1.0 } < Price { 1.0 - 2 * std::numeric_limits<double>::epsilon() });
     
-    CHECK(Price { 0.0 } > Price { -0.1 });
-    CHECK(Price { 0.0 } > Price { 0.0 - 2 * std::numeric_limits<double>::epsilon() });
-    CHECK_FALSE(Price { 0.0 } > Price { 0.1 });
-    CHECK_FALSE(Price { 0.0 } > Price { 0.0 + std::numeric_limits<double>::epsilon() });
-    CHECK_FALSE(Price { 0.0 } > Price { 0.0 - std::numeric_limits<double>::epsilon() });
-    CHECK_FALSE(Price { 0.0 } > Price { 0.0 + 2 * std::numeric_limits<double>::epsilon() });
+    CHECK(Price { 1.0 } > Price { 0.1 });
+    CHECK(Price { 1.0 } > Price { 1.0 - 2 * std::numeric_limits<double>::epsilon() });
+    CHECK_FALSE(Price { 1.0 } > Price { 2.0 });
+    CHECK_FALSE(Price { 1.0 } > Price { 1.0 + std::numeric_limits<double>::epsilon() });
+    CHECK_FALSE(Price { 1.0 } > Price { 1.0 - std::numeric_limits<double>::epsilon() });
+    CHECK_FALSE(Price { 1.0 } > Price { 1.0 + 2 * std::numeric_limits<double>::epsilon() });
     
-    CHECK(Price { 0.0 } <= Price { 0.0 });
-    CHECK(Price { 0.0 } <= Price { 0.1 });
-    CHECK(Price { 0.0 } <= Price { 0.0 - std::numeric_limits<double>::epsilon() });
-    CHECK(Price { 0.0 } <= Price { 0.0 + 2 * std::numeric_limits<double>::epsilon() });
-    CHECK_FALSE(Price { 0.0 } <= Price { -0.1 });
-    CHECK_FALSE(Price { 0.0 } <= Price { 0.0 - 2 * std::numeric_limits<double>::epsilon() });
+    CHECK(Price { 1.0 } <= Price { 1.0 });
+    CHECK(Price { 1.0 } <= Price { 1.1 });
+    CHECK(Price { 1.0 } <= Price { 1.0 - std::numeric_limits<double>::epsilon() });
+    CHECK(Price { 1.0 } <= Price { 1.0 + 2 * std::numeric_limits<double>::epsilon() });
+    CHECK_FALSE(Price { 1.0 } <= Price { 0.1 });
+    CHECK_FALSE(Price { 1.0 } <= Price { 1.0 - 2 * std::numeric_limits<double>::epsilon() });
     
-    CHECK(Price { 0.0 } >= Price { 0.0 });
-    CHECK(Price { 0.0 } >= Price { -0.1 });
-    CHECK(Price { 0.0 } >= Price { 0.0 + std::numeric_limits<double>::epsilon() });
-    CHECK(Price { 0.0 } >= Price { 0.0 - 2 * std::numeric_limits<double>::epsilon() });
-    CHECK_FALSE(Price { 0.0 } >= Price { 0.1 });
-    CHECK_FALSE(Price { 0.0 } >= Price { 0.0 + 2 * std::numeric_limits<double>::epsilon() });
+    CHECK(Price { 1.0 } >= Price { 1.0 });
+    CHECK(Price { 1.0 } >= Price { 0.1 });
+    CHECK(Price { 1.0 } >= Price { 1.0 + std::numeric_limits<double>::epsilon() });
+    CHECK(Price { 1.0 } >= Price { 1.0 - 2 * std::numeric_limits<double>::epsilon() });
+    CHECK_FALSE(Price { 1.0 } >= Price { 2.0 });
+    CHECK_FALSE(Price { 1.0 } >= Price { 1.0 + 2 * std::numeric_limits<double>::epsilon() });
 }
