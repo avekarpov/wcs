7eae7697f3b434ab39d1d1b7dea23f9cdd6728e8
Artem Karpov <artem.ka1001pm@gmail.com>
2023-04-30 19:10:17 +0300

    Add trade event parser

---
+++ wcs/include/wcs/trade_parser.hpp
@@ -0,0 +1,25 @@
+#ifndef WCS_TRADE_PARSER_HPP
+#define WCS_TRADE_PARSER_HPP
+
+#include "events/trade.hpp"
+#include "logger.hpp"
+
+namespace wcs
+{
+
+class TradeParser
+{
+public:
+    using Event = events::Trade;
+
+    static events::Trade parse(std::string_view event_string);
+
+private:
+    inline static Logger _logger { "TradeParser" };
+    
+};
+
+
+} // namespace wcs
+
+#endif //WCS_TRADE_PARSER_HPP
